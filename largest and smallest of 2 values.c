59e3d976aa9540dd22af5b532c4216b78c6c9402
rudra1551 <rudramehsana1551@gmail.com>
2025-08-28 14:43:20 +0530

    Add files via upload

---
+++ largest and smallest of 2 values.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+float a,b;
+printf("Enter Any Number:");
+scanf("%f",&a);
+printf("Enter Any Number:");
+scanf("%f",&b);
+if(a>b)
+{
+printf("a is largest and b is smallest");
+}
+else
+{
+printf("b is largest and a is smallest");
+}
+return 0;
+}
