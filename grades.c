0ea36a81c9997d9ee94bdbf61d37599e7fe82079
rudra1551 <rudramehsana1551@gmail.com>
2025-08-28 15:58:35 +0530

    Add files via upload

---
+++ grades.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+float sub1,sub2,sub3,total,average;
+printf("Enter marks of sub1:");
+scanf("%f",&sub1);
+printf("Enter marks of sub2:");
+scanf("%f",&sub2);
+printf("Enter marks of sub3:");
+scanf("%f",&sub3);
+total=sub1+sub2+sub3;
+average=total\3.0;
+if(sub1<35 || sub2<35 || sub3<35)
+{
+printf("result=fail);
+}
+else if(average>=70)
+printf()
