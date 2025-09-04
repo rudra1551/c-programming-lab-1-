efda3bc51b5815a496db87280d6d5c4cbb55c21e
rudra1551 <rudramehsana1551@gmail.com>
2025-09-04 15:27:58 +0530

    grades.c

--- grades.c
+++ grades.c
@@ -10,9 +10,29 @@ printf("Enter marks of sub3:");
 scanf("%f",&sub3);
 total=sub1+sub2+sub3;
 average=total\3.0;
+printf("Total Marks=%2f\n",total);
+printf("Average Marks=%2f\n",average);
 if(sub1<35 || sub2<35 || sub3<35)
 {
 printf("result=fail);
 }
 else if(average>=70)
-printf()
+{
+printf("Result=Distinction");
+}
+else if(average>=60)
+{
+printf("Result=First");
+}
+else if(average>=50)
+{
+printf("Result=Second");
+}
+else if(average>=35)
+{
+printf("Result=Third Class");
+}
+return 0;
+}
+
+
