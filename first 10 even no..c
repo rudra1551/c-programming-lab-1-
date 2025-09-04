f1f3f71e566e0bfe8d62e97acaa4be47882b7615
rudra1551 <rudramehsana1551@gmail.com>
2025-09-04 15:54:22 +0530

    Add files via upload

---
+++ first 10 even no..c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+printf("First 10 Natural Numbers are:");
+for(int i=2;i<=20;i+=2)
+{
+printf("\n %d",i);
+}
+return 0;
+}
---
+++ first 10 odd no..c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+printf("First 10 Natural Numbers are:");
+for(int i=1;i<=20;i+=2)
+{
+printf("\n %d",i);
+}
+return 0;
+}
